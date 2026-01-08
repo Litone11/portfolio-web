3eb200ea61b2c61b031331a1a77778e4df75ae93
Luís Martins <luis.a110686@gmail.com>
2026-01-08 00:45:05 +0000

    teste

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useMemo, useState } from "react";
 import "./App.css";
-import heroPortrait from "./assets/pp.jpeg";
+import heroPortrait from "./asset/pp.jpeg";
 
 const API = import.meta.env.VITE_API_URL;
 
